19a8f18cd01659c30daa7e7c7fa650509ef7498c
the-dugi <dejan.novak@gmx.net>
2023-11-07 15:59:28 +0100

    fix Sidekick

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,6 +13,28 @@ import {
   loadCSS,
 } from './aem.js';
 
+/**
+ * Event triggered from sidekick
+ * @param {object} detail details about the project
+ */
+const openDrive = ({ detail }) => {
+  const sk = detail.data;
+  window.open(sk?.config?.mountpoint, '_blank');
+};
+
+// bink event to the sidekick button
+const sk = document.querySelector('helix-sidekick');
+if (sk) {
+  // sidekick already loaded
+  sk.addEventListener('custom:open', openDrive);
+} else {
+  // wait for sidekick to be loaded
+  document.addEventListener('sidekick-ready', () => {
+    document.querySelector('helix-sidekick')
+      .addEventListener('custom:open', openDrive);
+  }, { once: true });
+}
+
 const LCP_BLOCKS = []; // add your LCP blocks to the list
 
 /**
